4c8c5d28a4176f8141216596390f939f86184ff8
oulala <oulala@qq.com>
2017-10-27 18:04:50 +0800

    add chain parameter max_vote_effect_times

--- libraries/chain/include/graphene/chain/config.hpp
+++ libraries/chain/include/graphene/chain/config.hpp
@@ -35,6 +35,8 @@
 #define GRAPHENE_MAX_SHARE_SUPPLY int64_t(1000000000000000ll)
 #define GRAPHENE_MAX_PAY_RATE 10000 /* 100% */
 #define GRAPHENE_MAX_SIG_CHECK_DEPTH 2
+
+#define GRAPHENE_MAX_VOTE_EFFECT_TIMES 100
 /**
  * Don't allow the committee_members to publish a limit that would
  * make the network unable to operate.
--- libraries/chain/include/graphene/chain/protocol/chain_parameters.hpp
+++ libraries/chain/include/graphene/chain/protocol/chain_parameters.hpp
@@ -69,6 +69,7 @@ namespace graphene { namespace chain {
       uint16_t                accounts_per_fee_scale              = GRAPHENE_DEFAULT_ACCOUNTS_PER_FEE_SCALE; ///< number of accounts between fee scalings
       uint8_t                 account_fee_scale_bitshifts         = GRAPHENE_DEFAULT_ACCOUNT_FEE_SCALE_BITSHIFTS; ///< number of times to left bitshift account registration fee at each scaling
       uint8_t                 max_authority_depth                 = GRAPHENE_MAX_SIG_CHECK_DEPTH;
+      uint16_t                max_vote_effect_times               = GRAPHENE_MAX_VOTE_EFFECT_TIMES;
       extensions_type         extensions;
 
       /** defined in fee_schedule.cpp */
@@ -106,5 +107,6 @@ FC_REFLECT( graphene::chain::chain_parameters,
             (accounts_per_fee_scale)
             (account_fee_scale_bitshifts)
             (max_authority_depth)
+            (max_vote_effect_times)
             (extensions)
           )
